cbb3b969bf37edd7e8cd537f1598ffd50379d35d
Bill Buff <bbuff27@gmail.com>
2020-11-23 19:54:53 -0700

    Make request to Star Wars api

--- lambdas/swapi/handler.js
+++ lambdas/swapi/handler.js
@@ -1,13 +1,25 @@
-import axios from 'axios';
 import { success, failure } from '../../util/responses';
+
+const axios = require('axios');
 
 export const main = async event => {
   const { SWAPI_BASE_URL } = process.env;
-  const eventBody = JSON.parse(event.body);
+  const { customerSelection } = event.Details.Parameters;
 
   try {
-    console.log(`Successful response: ${eventBody}`);
-    return success(eventBody);
+    const {
+      data: {
+        results
+      }
+    } = await axios({
+      url: `${SWAPI_BASE_URL}${customerSelection}`,
+      method: 'get',
+    });
+
+    if(results.length === 0) throw new Error(`Could not locate any ${customerSelection} for that selection`);
+
+    console.log(`Successful response: ${results}`);
+    return success(results);
   } catch (error) {
     console.error(error);
     return failure({ error });
