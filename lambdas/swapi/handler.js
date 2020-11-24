ff8606c98e82fc106445cd83366e50c27f36cbe5
Bill Buff <bbuff27@gmail.com>
2020-11-23 21:02:00 -0700

    Create attributes for contact flow

--- lambdas/swapi/handler.js
+++ lambdas/swapi/handler.js
@@ -4,7 +4,8 @@ const axios = require('axios');
 
 export const main = async event => {
   const { SWAPI_BASE_URL } = process.env;
-  const { customerSelection } = event.Details.Parameters;
+  const { customerSelection, pageNumber } = event.Details.Parameters;
+  const currentPage = (pageNumber ?? 0) + 1;
 
   try {
     const {
@@ -12,16 +13,24 @@ export const main = async event => {
         results
       }
     } = await axios({
-      url: `${SWAPI_BASE_URL}${customerSelection}`,
+      url: `${SWAPI_BASE_URL}${customerSelection}/?page=${currentPage}`,
       method: 'get',
     });
 
     if(results.length === 0) throw new Error(`Could not locate any ${customerSelection} for that selection`);
 
+    const responseBody = {
+      currentPage,
+      optionsString: buildAttributeString(results),
+      agentInfo: results,
+    }
+
     console.log(`Successful response: ${results}`);
-    return success(results);
+    return success(responseBody);
   } catch (error) {
     console.error(error);
     return failure({ error });
   }
 };
+
+const buildAttributeString = apiResponse => apiResponse.map(resource => resource.name).join(', ')
